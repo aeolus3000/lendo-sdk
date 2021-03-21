b4cf0578feb6bfcd8521dcec95b13fd6a04c1725
kopps <simon.kopp@thi.de>
2021-03-21 17:21:52 +0100

    add json utility

---
+++ utility/json.go
@@ -0,0 +1,13 @@
+package utility
+
+import (
+	"google.golang.org/protobuf/encoding/protojson"
+	"google.golang.org/protobuf/proto"
+)
+
+func MarshalToJson(message proto.Message) ([]byte, error) {
+	options := protojson.MarshalOptions{
+		UseProtoNames:     true,
+	}
+	return options.Marshal(message)
+}
